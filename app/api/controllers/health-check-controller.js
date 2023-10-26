edfd034c49f27ff5b17f6501737773e6fe25507a
Souvik Dinda <souvik.dinda.sd@gmail.com>
2023-10-26 14:12:56 -0400

    fix: Added logging to health check controller

--- app/api/controllers/health-check-controller.js
+++ app/api/controllers/health-check-controller.js
@@ -1,14 +1,18 @@
 import * as healthService from '../services/health-check-service.js';
+import logger from '../logger/index.js';
 
 export const getHealthCheck = async (req, res) => {
+    logger.info(`/GET/HealthController/Initiated`)
     if (req.body && Object.keys(req.body).length > 0 ||
     req.query && Object.keys(req.query).length > 0 || Object.keys(req.params).length > 0) {
         res.status(400).json();
     } else {
         const checkConnection = await healthService.checkConnection();
         if (checkConnection) {
+            logger.info(`/GET/HealthController/Success`)
             res.status(200).json();
         } else {
+            logger.error(`/GET/HealthController/Failure`)
             res.status(503).json();
         }
     }
